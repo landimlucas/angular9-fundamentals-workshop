7ff172fb3255511d9e1094fba7ae9c2d4aa562e1
Lucas Landim <landim.lucas@gmail.com>
2021-07-18 18:49:41 -0300

    update: api connection created

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -4,7 +4,7 @@ import { CoursesService } from '../shared/services/courses.service';
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
-  styleUrls: ['./courses.component.scss']
+  styleUrls: ['./courses.component.scss'],
 })
 export class CoursesComponent implements OnInit {
   // CHALLENGE
@@ -15,11 +15,11 @@ export class CoursesComponent implements OnInit {
 
   courses = null;
 
-  constructor(private coursesService: CoursesService) { }
+  constructor(private coursesService: CoursesService) {}
 
   ngOnInit(): void {
     this.resetSelectedCourse();
-    this.courses = this.coursesService.all();
+    this.loadCourses();
   }
 
   resetSelectedCourse() {
@@ -28,7 +28,7 @@ export class CoursesComponent implements OnInit {
       title: '',
       description: '',
       percentComplete: 0,
-      favorite: false
+      favorite: false,
     };
 
     this.selectedCourse = emptyCourse;
@@ -38,11 +38,17 @@ export class CoursesComponent implements OnInit {
     this.selectedCourse = course;
   }
 
+  loadCourses() {
+    this.coursesService.all().subscribe((courses) => (this.courses = courses));
+  }
+
   saveCourse(course) {
-    if(course.id) {
+    if (course.id) {
       this.coursesService.update(course);
     } else {
-      this.coursesService.create(course);
+      this.coursesService
+        .create(course)
+        .subscribe((result) => this.loadCourses());
     }
   }
 
--- src/app/shared/services/courses.service.ts
+++ src/app/shared/services/courses.service.ts
@@ -1,38 +1,41 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
+const BASE_URL = 'http://localhost:3000/';
+
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CoursesService {
+  private model = 'courses';
+
   private courses = [
     {
       id: 1,
       title: 'Angular 9 Fundamentals!!',
       description: 'Learn the fundamentals of Angular 9',
       percentComplete: 26,
-      favorite: true
+      favorite: true,
     },
     {
       id: 2,
       title: 'JavaScript The Really REALLY HARD PARTS!!',
       description: 'Worship Will Sentance',
       percentComplete: 50,
-      favorite: true
-    }
+      favorite: true,
+    },
   ];
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
 
   all() {
-    return this.courses;
+    return this.http.get(this.getUrl());
   }
 
-  find(courseId) {
-
-  }
+  find(courseId) {}
 
   create(course) {
-    console.log('CREATE COURSE', course);
+    return this.http.post(this.getUrl(), course);
   }
 
   update(course) {
@@ -41,5 +44,9 @@ export class CoursesService {
 
   delete(courseId) {
     console.log('DLETE COURSE', courseId);
+  }
+
+  private getUrl() {
+    return `${BASE_URL}${this.model}`;
   }
 }
